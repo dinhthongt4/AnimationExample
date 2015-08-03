ba1115d7fc1eb0d54f23af458da8a9cad6d2f381
Thong <dinhthongt4@gmail.com>
2015-08-03 14:42:21 +0700

    Create AccelerateActivity

---
+++ app/src/main/java/com/example/thong/animationexample/AccelerateActivity.java
@@ -0,0 +1,12 @@
+package com.example.thong.animationexample;
+
+import android.app.Activity;
+
+import org.androidannotations.annotations.EActivity;
+
+/**
+ * Created by thong on 03/08/2015.
+ */
+@EActivity(R.layout.activity_acde)
+public class AccelerateActivity extends Activity{
+}
--- app/src/main/java/com/example/thong/animationexample/MainActivity.java
+++ app/src/main/java/com/example/thong/animationexample/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.thong.animationexample;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends Activity {
 
+    @ViewById(R.id.btnAcDe)
+    Button mBtnAcDe;
+
+    @Click(R.id.btnAcDe)
+    void intentAcDeExample() {
+        Intent intent = new Intent(this,AccelerateActivity_.class);
+        startActivity(intent);
+    }
 }
