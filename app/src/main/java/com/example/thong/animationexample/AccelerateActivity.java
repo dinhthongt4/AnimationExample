8434cb2e4efbe26c8500d838779ba70eaa512af3
Thong <dinhthongt4@gmail.com>
2015-08-03 15:14:03 +0700

    finish accelerate example

--- app/src/main/java/com/example/thong/animationexample/AccelerateActivity.java
+++ app/src/main/java/com/example/thong/animationexample/AccelerateActivity.java
@@ -1,12 +1,24 @@
 package com.example.thong.animationexample;
 
 import android.app.Activity;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 /**
  * Created by thong on 03/08/2015.
  */
 @EActivity(R.layout.activity_acde)
-public class AccelerateActivity extends Activity{
+public class AccelerateActivity extends Activity {
+
+    @ViewById(R.id.button)
+    Button button;
+
+    @Click(R.id.button)
+    void setAccelerateActivity() {
+        button.startAnimation(AnimationUtils.loadAnimation(AccelerateActivity.this,R.anim.scale_button));
+    }
 }
